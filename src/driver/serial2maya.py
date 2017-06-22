db013c655e8cb2711eb2f37cfc793ccc922f9432
giuliom95 <giuliomartella1995@gmail.com>
2017-06-22 20:31:09 +0200

    Added stdout messages to the driver

--- src/driver/serial2maya.py
+++ src/driver/serial2maya.py
@@ -5,9 +5,16 @@ MAYA_ADDR = ('127.0.0.1', 1923)
 ARDUINO_PORT = '/dev/ttyACM0'
 
 if __name__ == '__main__':
+    print('Welcome to the arduino-maya serial driver.')
+    print('Maya address: {0}:{1}'.format(*MAYA_ADDR))
+    print('Arduino port: {0}'.format(ARDUINO_PORT))
+
     mayaconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     mayaconn.connect(MAYA_ADDR)
+    print('Maya connection established.')
+
     arduinoconn = serial.Serial(ARDUINO_PORT)
+    print('Arduino connection established.')
 
     while True:
         delta = arduinoconn.readline().rstrip()
@@ -21,4 +28,7 @@ if __name__ == '__main__':
                 mayaconn.send(cmd)
 
     arduinoconn.close()
+    print('Arduino connection closed.')
+
     mayaconn.close()
+    print('Maya connection closed.')
