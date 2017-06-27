a158d7715d2e49d01172da7fcc726b074b723937
giuliom95 <giuliomartella1995@gmail.com>
2017-06-27 23:20:31 +0200

    Better info and error messages

--- src/driver/serial2maya.py
+++ src/driver/serial2maya.py
@@ -1,20 +1,33 @@
 import socket
 import serial
+import time
 
 MAYA_ADDR = ('127.0.0.1', 1923)
 ARDUINO_PORT = '/dev/ttyACM0'
 
 if __name__ == '__main__':
     print('Welcome to the arduino-maya serial driver.')
-    print('Maya address: {0}:{1}'.format(*MAYA_ADDR))
-    print('Arduino port: {0}'.format(ARDUINO_PORT))
+    print('Press CTRL-C to exit.')
+    print('')
+    print('[INFO] Maya address: {0}:{1}'.format(*MAYA_ADDR))
+    print('[INFO] Arduino port: {0}'.format(ARDUINO_PORT))
 
-    mayaconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    mayaconn.connect(MAYA_ADDR)
-    print('Maya connection established.')
+    mayaconn = None
+    while mayaconn is None:
+        try:
+            mayaconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            mayaconn.connect(MAYA_ADDR)
+        except:
+            mayaconn = None
+            print('[ERROR] Maya connection refused.')
+            print('[ERROR] (Maybe you didn\'t load the plugin in Maya?)')
+            print('[INFO] Trying again in 5 seconds...')
+            time.sleep(5)
+
+    print('[INFO] Maya connection established.')
 
     arduinoconn = serial.Serial(ARDUINO_PORT)
-    print('Arduino connection established.')
+    print('[INFO] Arduino connection established.')
 
     while True:
         deltas = arduinoconn.readline().split()
@@ -29,7 +42,7 @@ if __name__ == '__main__':
                     mayaconn.send(cmd)
 
     arduinoconn.close()
-    print('Arduino connection closed.')
+    print('[INFO] Arduino connection closed.')
 
     mayaconn.close()
-    print('Maya connection closed.')
+    print('[INFO] Maya connection closed.')
