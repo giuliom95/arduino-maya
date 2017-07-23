240d735eb3e059dd1b68778ea1c8106b99e28a96
giuliom95 <giuliomartella1995@gmail.com>
2017-07-23 13:52:46 +0200

    Error handling corrections

--- install-maya-plug.sh
+++ install-maya-plug.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 PLUGIN_PATH=$HOME"/maya/plug-ins"
-cp src/maya/*.py $PLUGIN_PATH
+cp -v src/maya/*.py $PLUGIN_PATH
--- src/maya/arduinomaya.py
+++ src/maya/arduinomaya.py
@@ -10,7 +10,8 @@ from shiboken2 import wrapInstance
 CHANNELS_NUM = 3
 CHANNEL_MAX = 1023.
 
-class Channel():
+
+class Channels():
     channels = [{}]*CHANNELS_NUM
 
 
@@ -37,9 +38,9 @@ class ConnectAttributeCommand(OpenMaya.MPxCommand):
         return ConnectAttributeCommand()
 
     def doIt(self, args):
-        syntax = 'Syntax: <channel>, <object name>, <object attribute>, <min value>, <max value>'
+        syntax = 'Syntax: <channel> <object name> <object attribute> <min value> <max value>'
         if len(args) != 5:
-            raise ValueError('Wrong arguments number. ' + syntax)
+            raise RuntimeError('Wrong arguments number. ' + syntax)
 
         try:
             channel = args.asInt(0)
@@ -48,23 +49,24 @@ class ConnectAttributeCommand(OpenMaya.MPxCommand):
             minVal = args.asFloat(3)
             maxVal = args.asInt(4)
         except:
-            raise ValueError('Invalid arguments. ' + syntax)
+            raise RuntimeError('Invalid arguments. ' + syntax)
 
         if channel > CHANNELS_NUM or channel < 0:
-            raise ValueError('Invalid channel number. ' + syntax)
+            raise RuntimeError('Invalid channel number. ' + syntax)
 
         try:
             obj = pmc.ls(objName)[0]
         except:
-            raise ValueError('No object called ' + objName)
+            raise RuntimeError('No object called ' + objName)
 
         if not obj.hasAttr(objAttr):
-            raise ValueError('No attribute of ' + objName + ' called ' + objAttr)
+            raise RuntimeError('No attribute of ' + objName + ' called ' + objAttr)
 
         if type(obj.getAttr(objAttr)) != float:
-            raise ValueError('Attribute ' + objAttr + ' of ' + objName + ' is not a float.')
+            raise RuntimeError('Attribute ' + objAttr + ' of ' + objName + ' is not a float.')
 
-        Channel.channels[channel] = {'obj': objName, 'attr': objAttr, 'min': minVal, 'max': maxVal}
+        Channels.channels[channel] = {'obj': objName, 'attr': objAttr, 'min': minVal, 'max': maxVal}
+
 
 class ConnectTimeCommand(OpenMaya.MPxCommand):
     commandName = 'arduinoConnectTime'
@@ -79,17 +81,17 @@ class ConnectTimeCommand(OpenMaya.MPxCommand):
     def doIt(self, args):
         syntax = 'Syntax: channel.'
         if len(args) != 1:
-            raise ValueError('Wrong arguments number. ' + syntax)
+            raise RuntimeError('Wrong arguments number. ' + syntax)
 
         try:
             channel = args.asInt(0)
         except:
-            raise ValueError('Invalid channel. ' + syntax)
+            raise RuntimeError('Invalid channel. ' + syntax)
 
         if channel > CHANNELS_NUM or channel < 0:
-            raise ValueError('Invalid channel number. ' + syntax)
+            raise RuntimeError('Invalid channel number. ' + syntax)
 
-        Channel.channels[channel] = 'time'
+        Channels.channels[channel] = 'time'
 
 
 class UpdateChannelCommand(OpenMaya.MPxCommand):
@@ -104,20 +106,29 @@ class UpdateChannelCommand(OpenMaya.MPxCommand):
 
     def doIt(self, args):
         channel = args.asInt(0)
-        value = args.asInt(1)
 
-        if Channel.channels[channel] == 'time':
+        try:
+            value = args.asInt(1)
+        except TypeError:
+            # We ignore broken data incoming from the driver
+            pass
+
+        if Channels.channels[channel] == 'time':
             newT = pmc.currentTime() + value
             pmc.currentTime(newT)
         else:
-            ch = Channel.channels[channel]
-            objName = ch['obj']
-            objAttr = ch['attr']
-            if objName == '':
-                return
-            obj = pmc.ls(objName)[0]
-            newValue = (ch['max']-ch['min'])*(value / CHANNEL_MAX) + ch['min']
-            obj.setAttr(objAttr, newValue)
+            try:
+                ch = Channels.channels[channel]
+                objName = ch['obj']
+                objAttr = ch['attr']
+                if objName == '':
+                    return
+                obj = pmc.ls(objName)[0]
+                newValue = (ch['max']-ch['min'])*(value / CHANNEL_MAX) + ch['min']
+                obj.setAttr(objAttr, newValue)
+            except KeyError:
+                # This should happen only if channel has not been set up
+                pass
 
 
 class GUIControlsCommand(OpenMaya.MPxCommand):
@@ -135,13 +146,13 @@ class GUIControlsCommand(OpenMaya.MPxCommand):
     def doIt(self, args):
         syntax = GUIControlsCommand.syntax
         if len(args) != 1:
-            raise ValueError('Wrong arguments number. ' + syntax)
+            raise RuntimeError('Wrong arguments number. ' + syntax)
 
         cmd = args.asString(0)
         if cmd == 'show':
             self.window.show()
         else:
-            raise ValueError('Command not recognized.')
+            raise RuntimeError('Command not recognized.')
 
 
 ##########################################################
@@ -181,7 +192,7 @@ def initializePlugin(mobject):
         sys.stderr.write(
             'Failed to register command: ' + UpdateChannelCommand.commandName)
         raise
-    
+
     try:
         mplugin.registerCommand(
             GUIControlsCommand.commandName,
